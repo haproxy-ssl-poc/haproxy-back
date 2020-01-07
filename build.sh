4c47bc9bf4deeda00d96cb0bd905b7338065ba99
Abderrazak BOUADMA <abderrazak.bouadma@kyriba.com>
2020-01-07 16:35:46 +0100

    clean

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn -DskipTests clean package  jib:build -Dimage=abderrazak/kyr-greetings:1.0.0-SNAPSHOT
+mvn -DskipTests clean package  -Dimage=abderrazak/kyr-greetings:1.0.0-SNAPSHOT
 docker pull abderrazak/kyr-greetings:1.0.0-SNAPSHOT
