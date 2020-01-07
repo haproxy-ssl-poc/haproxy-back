9ecea38551eee01a4d49f64b7f16f7994622b294
Abderrazak BOUADMA <abderrazak.bouadma@kyriba.com>
2020-01-07 16:36:45 +0100

    clean

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 set e
 
+docker pull abderrazak/kyr-greetings:1.0.0-SNAPSHOT
 docker run -d --network ha --rm -v$(pwd)/certs:/etc/certs -p8443:443 -p8090:8080 --name greetings1 abderrazak/kyr-greetings:1.0.0-SNAPSHOT
 docker run -d --network ha --rm -v$(pwd)/certs:/etc/certs -p9443:443 -p8091:8080 --name greetings2 abderrazak/kyr-greetings:1.0.0-SNAPSHOT
 
